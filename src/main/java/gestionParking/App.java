d7080e10226743b15cf6a8ef1b60bfbdcfc89b19
@k€€m <akeem.baha@gmail.com>
2020-02-12 23:30:48 +0100

    +methode autorisation + creations de listes de voitures + scenario

--- src/main/java/gestionParking/App.java
+++ src/main/java/gestionParking/App.java
@@ -9,108 +9,23 @@ public final class App {
     private App() {
     }
 
-    static String vehiculeAutorise[] = { "XX-777-ZZ", "DD-222-VV", "AA-444-ZC" };
-    static String vehiculeStationne[] = { "XX-777-ZZ" };
-
     /**
      * @param args The arguments of the program.
      */
     public static void main(final String[] args) {
 
-        Vehicule v1 = new Vehicule("XX-222-DD", false);
-        Vehicule v2 = new Vehicule("YY-232-DD", true);
-        Vehicule v3 = new Vehicule("FF-445-DD", true);
-        Vehicule v4 = new Vehicule("DD-122-DD", false);
-        Vehicule v5 = new Vehicule("CC-377-DD", true);
-        Vehicule essai = new Vehicule("dd-549-jj", false);
-        Vehicule essai2 = new Vehicule("DC-899-11", false);
-
-        List<Vehicule> voitureGaree = new ArrayList<Vehicule>();
-
-        List<Vehicule> listVehicule = new ArrayList<Vehicule>();
-        listVehicule.add(v1);
-        listVehicule.add(v2);
-        listVehicule.add(v3);
-        listVehicule.add(v4);
-        listVehicule.add(v5);
-        listVehicule.add(essai);
-        
-
-
-        // System.out.println(listVehicule);
-
-        /**
-         * entrée des voitures sur le parking, isStationne = true
-         */
-        for (int i = 0; i < listVehicule.size(); i++) {
-
-            if(listVehicule.get(i).getIsStationne())
-            {
-                voitureGaree.add(listVehicule.get(i));
-            }
-        } 
-        /**
-         * récupération des véhicules garées (true)
-         */
-        for(int i = 0;  i < voitureGaree.size(); i++)  
-        {
-            System.out.println(voitureGaree.get(i).getImmatriculation());
-        }  
-        System.out.println();
-        /**
-         * ajout du véhicule essai sur le parking,
-         * s'il fait partie de la liste, le laisser entrer
-        */   
-        if(listVehicule.contains(essai))
-        {
-            essai.setIsStationne(true);;
-        }
-        else
-        {
-            System.out.println(essai.getImmatriculation() + " : Vous n'êtes pas autorisé à rentrer");
-        }
-        /**
-         * Nouvelle récupération des véhicules garées (true)
-         */
-        for (int i = 0; i < listVehicule.size(); i++) {
-            if(listVehicule.get(i).getIsStationne())
-            {
-                System.out.println(listVehicule.get(i).getImmatriculation());
-            }
-        } 
-        System.out.println();
-
-        /**
-         * ajout du véhicule essai2 sur le parking,
-         * s'il fait partie de la liste, le laisser entrer
-        */   
-        if( listVehicule.contains(essai2))
-        {
-            essai2.setIsStationne(true);;
-        }
-        else
-        {
-            System.out.println(essai2.getImmatriculation() + " : Vous n'êtes pas autorisé à rentrer");
-        }
-     
-        
-         salutation();   
-  
         Parking parking = new Parking(30,"Valenciennes");
         List<Vehicule> voituresAdmissible = parking.CreerListeVoitures();
-        System.out.println(voituresAdmissible.get(5).getImmatriculation());
-        Boolean essai1 = parking.AutoriserAcces(voituresAdmissible.get(2), voituresAdmissible);
-        System.out.println(essai1);
-    }
-    
-    /////////////// Fin de main
-    /////////////// !!!!///////////////////////////////////////////////////
-    public static void salutation() 
-    {
-        
-        System.out.println("salut lami");
+        List<Vehicule> listeVoitures = parking.CreerListeVoituresComplètes();
 
-         
+        Boolean essai1 = parking.AutoriserAcces(listeVoitures.get(2), voituresAdmissible);
+        Boolean essai2 = parking.AutoriserAcces(listeVoitures.get(3), voituresAdmissible);
+        Boolean essai5 = parking.AutoriserAcces(listeVoitures.get(5), voituresAdmissible);
+        Boolean essai3 = parking.AutoriserAcces(listeVoitures.get(6), voituresAdmissible);
+        Boolean essai8 = parking.AutoriserAcces(listeVoitures.get(8), voituresAdmissible);
+        Boolean essai4 = parking.AutoriserAcces(listeVoitures.get(9), voituresAdmissible);
+        Boolean essai7 = parking.AutoriserAcces(listeVoitures.get(10), voituresAdmissible);
+        parking.AfficherVoituresParking(voituresAdmissible);
     }
 
 }
--- src/main/java/gestionParking/Parking.java
+++ src/main/java/gestionParking/Parking.java
@@ -24,17 +24,26 @@ class Parking {
         this.nbPlaceMax = nbPlaceMax;
         this.ville = ville;
     }
+    Vehicule v1 = new Vehicule("CX-272-DE", false);
+    Vehicule v2 = new Vehicule("YS-232-ZD", false);
+    Vehicule v3 = new Vehicule("SF-145-ED", false);
+    Vehicule v4 = new Vehicule("DD-120-SS", false);
+    Vehicule v5 = new Vehicule("IC-377-WD", false);
     Vehicule v110 = new Vehicule("XE-222-HD", false);
     Vehicule v111 = new Vehicule("XX-255-AD", false);
-    Vehicule v120 = new Vehicule("YE-232-VD", true);
-    Vehicule v121 = new Vehicule("YY-292-CQ", true);
-    Vehicule v130 = new Vehicule("FE-995-ZW", true);
-    Vehicule v131 = new Vehicule("FF-445-DD", true);
+    Vehicule v120 = new Vehicule("YE-232-VD", false);
+    Vehicule v121 = new Vehicule("YY-292-CQ", false);
+    Vehicule v130 = new Vehicule("FE-995-ZW", false);
+    Vehicule v131 = new Vehicule("FF-445-DD", false);
     Vehicule v140 = new Vehicule("DE-122-ZD", false);
     Vehicule v141 = new Vehicule("DD-128-SD", false);
-    Vehicule v150 = new Vehicule("CE-377-ZD", true);
-    Vehicule v151 = new Vehicule("CC-370-CD", true);
+    Vehicule v150 = new Vehicule("CE-377-ZD", false);
+    Vehicule v151 = new Vehicule("CC-370-CD", false);
 
+    /**
+     * je créer 1 liste de voitures : admises 
+     * @return
+     */
     public List CreerListeVoitures() {
         List<Vehicule> vehiculeAdmis = new ArrayList<Vehicule>();
         vehiculeAdmis.add(v110);
@@ -43,8 +52,34 @@ class Parking {
         vehiculeAdmis.add(v121);
         vehiculeAdmis.add(v130);
         vehiculeAdmis.add(v131);
+        vehiculeAdmis.add(v150);
+        vehiculeAdmis.add(v151);
+        vehiculeAdmis.add(v1);
+        vehiculeAdmis.add(v2);
+        vehiculeAdmis.add(v5);
 
         return vehiculeAdmis;
+    }
+    public List CreerListeVoituresComplètes() {
+        List<Vehicule> listeVehicules = new ArrayList<Vehicule>();
+
+        listeVehicules.add(v1);
+        listeVehicules.add(v2);
+        listeVehicules.add(v3);
+        listeVehicules.add(v4);
+        listeVehicules.add(v5);
+        listeVehicules.add(v110);
+        listeVehicules.add(v111);
+        listeVehicules.add(v120);
+        listeVehicules.add(v121);
+        listeVehicules.add(v130);
+        listeVehicules.add(v131);
+        listeVehicules.add(v140);
+        listeVehicules.add(v141);
+        listeVehicules.add(v150);
+        listeVehicules.add(v151);
+
+        return listeVehicules;
     }
   
     public Boolean AutoriserAcces(Vehicule vehicule, List vehiculeAdmis){
@@ -52,14 +87,31 @@ class Parking {
         if(vehiculeAdmis.contains(vehicule))
         {
             vehicule.setIsStationne(true);
+            System.out.println("//////////////////////////////////////////");
             System.out.println("le véhicule " + vehicule.getImmatriculation() + " entre dans le parking");
+            System.out.println();
             return true;
         }
         else
         {
-            System.out.println("vous n'êtes pas autoriser à rentrer");
+            System.out.println("//////////////////////////////////////////");
+            System.out.println(vehicule.getImmatriculation() + " !!! Vous n'êtes pas autoriser à rentrer");
+            System.out.println();
+
             return false;
         }
+    }
+    public void AfficherVoituresParking(List<Vehicule> vehiculesAdmis) {
+        List<Vehicule> voitureGaree = new ArrayList<Vehicule>();
+        System.out.println("****Vehicules actuellement garés****");
+        for (int i = 0; i < vehiculesAdmis.size(); i++) {
+            if(vehiculesAdmis.get(i).getIsStationne())
+            {
+                voitureGaree.add(vehiculesAdmis.get(i));
+                System.out.println( i + " : " + vehiculesAdmis.get(i).getImmatriculation());
+            }
+        }
+        
     }
 
 }
